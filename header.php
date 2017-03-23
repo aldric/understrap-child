ed71d5d01ef0034ec4ecb6069c5588532d43ed01
Aldric Gaudinot <gaudinot@gmail.com>
2017-03-23 22:04:40 +0100

    side bars

--- header.php
+++ header.php
@@ -25,53 +25,14 @@ $container = get_theme_mod('understrap_container_type');
 
 <body <?php body_class(); ?>>
 
-<div class="hfeed site" id="page">
+<div class="hfeed site layout-container" id="page">
 
 	<!-- ******************* The Navbar Area ******************* -->
 	<div class="wrapper-fluid wrapper-navbar" id="wrapper-navbar">
 
 		<a class="skip-link screen-reader-text sr-only" href="#content"><?php esc_html_e('Skip to content', 'understrap'); ?></a>
 
-		<nav class="navbar navbar-toggleable-md  navbar-light">
 
-				<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-
-					<!-- Your site title as branding in the menu -->
-					<?php if (! has_custom_logo()) {
-    ?>
-
-						<?php if (is_front_page() && is_home()) : ?>
-
-							<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>"><?php bloginfo('name'); ?></a></h1>
-
-						<?php else : ?>
-
-							<a class="navbar-brand" rel="home" href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>"><?php bloginfo('name'); ?></a>
-
-						<?php endif; ?>
-
-
-					<?php
-
-} else {
-    the_custom_logo();
-} ?><!-- end custom logo -->
-				<!-- The WordPress Menu goes here -->
-				<?php wp_nav_menu(
-                    array(
-                        'theme_location'  => 'primary',
-                        'container_class' => 'collapse navbar-collapse',
-                        'container_id'    => 'navbarNavDropdown',
-                        'menu_class'      => 'navbar-nav mr-auto',
-                        'fallback_cb'     => '',
-                        'menu_id'         => 'main-menu',
-                        'walker'          => new WP_Bootstrap_Navwalker(),
-                    )
-                ); ?>
-
-		</nav><!-- .site-navigation -->
 	</div><!-- .wrapper-navbar end -->
 		<!-- breadcrumbs widget location -->
 		<?php
@@ -87,4 +48,3 @@ $container = get_theme_mod('understrap_container_type');
           <i class="fa fa-angle-double-up"></i>
         </a>
 			</div>
-
--- src/js/custom/main.js
+++ src/js/custom/main.js
@@ -1,84 +1,84 @@
 var BreadcrumbGenerator = function() {
-	var self = this;
-	
-	this.breadCrumb = {
-   		"@context": "http://schema.org",
-   		"@type": "BreadcrumbList",
-   		"itemListElement": [ ]
-   	};
+    var self = this;
 
-	this.populate = function() {
-		var idx = 0;
-		jQuery('nav.breadcrumb a.breadcrumb-item').each(function( index ) {
-				idx = index + 1;
-				var text = idx == 1 ? jQuery(this).find("meta").prop("content") : jQuery(this).text();
-				text = text.charAt(0).toUpperCase() + text.slice(1);
-				
-				self.breadCrumb.itemListElement.push(
-					self.getItem(idx, jQuery(this).attr("href"), text)
-				);
-				
-			});
-		jQuery('nav.breadcrumb span.breadcrumb-item.active').each(function(index) {
-				var text = jQuery(this).find('span[property=name]').text();
-				if(text === '')
-					text = jQuery(this).find("meta").prop("content");
-				self.breadCrumb.itemListElement.push(
-					self.getItem(idx + index + 1, window.location.href, text)
-				);
-			});
-		
-	};
-	
-	this.generate = function() { 
-		return JSON.stringify(self.breadCrumb);
-	};
+    this.breadCrumb = {
+        "@context": "http://schema.org",
+        "@type": "BreadcrumbList",
+        "itemListElement": []
+    };
 
-	this.isEmpty = function() {
-		return self.breadCrumb.itemListElement.length < 2;
-	};
+    this.populate = function() {
+        var idx = 0;
+        jQuery('nav.breadcrumb a.breadcrumb-item').each(function(index) {
+            idx = index + 1;
+            var text = idx == 1 ? jQuery(this).find("meta").prop("content") : jQuery(this).text();
+            text = text.charAt(0).toUpperCase() + text.slice(1);
 
-	this.getItem = function(position, link, text) {
-		return 	{
-					"@type": "ListItem",
-					"position": position,
-					"item":
-						{
-							"@id": link ,
-							"name": text
-						}
-				};
-	};
+            self.breadCrumb.itemListElement.push(
+                self.getItem(idx, jQuery(this).attr("href"), text)
+            );
 
-	this.createJsonLdTag = function() {
-		jQuery("body").append(jQuery("<script></script>").attr("type","application/ld+json").text(this.generate()));
-	};
+        });
+        jQuery('nav.breadcrumb span.breadcrumb-item.active').each(function(index) {
+            var text = jQuery(this).find('span[property=name]').text();
+            if (text === '')
+                text = jQuery(this).find("meta").prop("content");
+            self.breadCrumb.itemListElement.push(
+                self.getItem(idx + index + 1, window.location.href, text)
+            );
+        });
+
+    };
+
+    this.generate = function() {
+        return JSON.stringify(self.breadCrumb);
+    };
+
+    this.isEmpty = function() {
+        return self.breadCrumb.itemListElement.length < 2;
+    };
+
+    this.getItem = function(position, link, text) {
+        return {
+            "@type": "ListItem",
+            "position": position,
+            "item": {
+                "@id": link,
+                "name": text
+            }
+        };
+    };
+
+    this.createJsonLdTag = function() {
+        jQuery("body").append(jQuery("<script></script>").attr("type", "application/ld+json").text(this.generate()));
+    };
 }
 
 function debounce(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
+    var timeout;
+    return function() {
+        var context = this,
+            args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
 };
 
-jQuery(document).on('ready', function () { 
-	var breadcrumbGenerator = new BreadcrumbGenerator();
-	breadcrumbGenerator.populate();
-	if(!breadcrumbGenerator.isEmpty())
-		breadcrumbGenerator.createJsonLdTag();
-	else {
-		jQuery('div#wrapper-breadcrumbs').hide(0);
-	}
-	showHideGoToTop();
+jQuery(document).on('ready', function() {
+    var breadcrumbGenerator = new BreadcrumbGenerator();
+    breadcrumbGenerator.populate();
+    if (!breadcrumbGenerator.isEmpty())
+        breadcrumbGenerator.createJsonLdTag();
+    else {
+        jQuery('div#wrapper-breadcrumbs').hide(0);
+    }
+    showHideGoToTop();
 });
 
 
@@ -91,13 +91,13 @@ jQuery('a#go-to-top').click(function() {
 });
 
 function showHideGoToTop() {
-	if(jQuery(window).scrollTop() < 200) {
-		jQuery('a#go-to-top').hide(150, 'linear');
-	} else {
-		jQuery('a#go-to-top').show(150, 'linear');
-	}
+    if (jQuery(window).scrollTop() < 200) {
+        jQuery('a#go-to-top').hide(150, 'linear');
+    } else {
+        jQuery('a#go-to-top').show(150, 'linear');
+    }
 }
 
 jQuery(window).on('scroll', debounce(function(e) {
-	showHideGoToTop();
+    showHideGoToTop();
 }, 500));
